283b32f727cbc110e38e33204cdea3ec4a63618c
Benric08 <richy8.26.05@gmail.com>
2023-06-09 18:59:22 -0500

    correccion de los endopoints para produccion

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import Fileview from './components/forms/Fileview';
 import { Callendar } from './components/containers/Callendar';
 import { Box } from '@mui/material';
 import axios from 'axios';
-//axios.defaults.baseURL="https://sistemarequerimiento-production.up.railway.app/"
-axios.defaults.baseURL="http://localhost:3001/"
+axios.defaults.baseURL="https://sistemarequerimiento-production.up.railway.app/"
+//axios.defaults.baseURL="http://localhost:3001/"
 
 function App() {
   return (
